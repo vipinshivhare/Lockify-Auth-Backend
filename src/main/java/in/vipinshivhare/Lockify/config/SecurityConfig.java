d912a474bc7c6c38b3bbbaf0e40c2df8718d90a0
vipinshivhare <vipinshivhare804@gmail.com>
2025-07-24 17:27:46 +0530

    Initial

--- src/main/java/in/vipinshivhare/Lockify/config/SecurityConfig.java
+++ src/main/java/in/vipinshivhare/Lockify/config/SecurityConfig.java
@@ -63,7 +63,10 @@ public class SecurityConfig {
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 //      config.setAllowedOrigins(List.of("http://localhost:5173"));
-        config.setAllowedOrigins(List.of(" https://lockify-client.netlify.app"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://lockify-client.netlify.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*") ); // Allow all headers for flexibility
         config.setAllowCredentials(true);
