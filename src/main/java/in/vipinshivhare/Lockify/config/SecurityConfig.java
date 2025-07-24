53810e51062bf2e195695f2b3e98cb2cc5964460
vipinshivhare <vipinshivhare804@gmail.com>
2025-07-24 17:01:33 +0530

    adding backend url

--- src/main/java/in/vipinshivhare/Lockify/config/SecurityConfig.java
+++ src/main/java/in/vipinshivhare/Lockify/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+//      config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(" https://lockify-client.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*") ); // Allow all headers for flexibility
         config.setAllowCredentials(true);
